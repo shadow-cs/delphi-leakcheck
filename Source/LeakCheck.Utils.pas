3fb3dc0676dec560d22aea6eac6104a30c15f8a7
Honza Rames <ramejan@gmail.com>
2015-03-09 14:20:27 +0100

    reworked IgnoreManagedFields to match how System does it even more, added few other ignoring utility function

--- Source/LeakCheck.Utils.pas
+++ Source/LeakCheck.Utils.pas
@@ -27,7 +27,8 @@ unit LeakCheck.Utils;
 interface
 
 uses
-  LeakCheck;
+  LeakCheck,
+  System.Classes;
 
 
 /// <summary>
@@ -38,15 +39,35 @@ function IgnoreRttiObjects(const Instance: TObject; ClassType: TClass): Boolean;
 
 /// <summary>
 ///   When assigned to <see cref="LeakCheck|TLeakCheck.InstanceIgnoredProc" />
+///   it ignores <c>TCustomAttribute</c> instances.
+/// </summary>
+function IgnoreCustomAttributes(const Instance: TObject; ClassType: TClass): Boolean;
+
+/// <summary>
+///   When assigned to <see cref="LeakCheck|TLeakCheck.InstanceIgnoredProc" />
+///   it ignores classes that the compiler creates for anonymous methods.
+/// </summary>
+function IgnoreAnonymousMethodPointers(const Instance: TObject; ClassType: TClass): Boolean;
+
+/// <summary>
+///   When assigned to <see cref="LeakCheck|TLeakCheck.InstanceIgnoredProc" />
 ///   it ignores multiple objects by calling all registered methods.
 /// </summary>
 function IgnoreMultipleObjects(const Instance: TObject; ClassType: TClass): Boolean;
 
-procedure AddIgnoreObjectProc(Proc: TLeakCheck.TIsInstanceIgnored);
+procedure AddIgnoreObjectProc(Proc: TLeakCheck.TIsInstanceIgnored); overload;
+procedure AddIgnoreObjectProc(const Procs: array of TLeakCheck.TIsInstanceIgnored); overload;
+
+procedure IgnoreStrings(const Strings: TStrings);
 
 /// <summary>
 ///   Ignore managed fields that may leak in given object instance.
 /// </summary>
+/// <remarks>
+///   Note that only a subset of managed fields is supported, this should be
+///   used for high-level testing. If you need to ignore more types, it is
+///   suggested to add <c>tkUnknown</c> to global ignore.
+/// </remarks>
 procedure IgnoreManagedFields(const Instance: TObject; ClassType: TClass);
 
 implementation
@@ -55,6 +76,7 @@ uses
 {$IFDEF POSIX}
   Posix.Proc,
 {$ENDIF}
+  System.SysUtils,
   System.TypInfo,
   System.Rtti;
 
@@ -74,40 +96,121 @@ type
     Fields: array [0..0] of TFieldInfo;
   end;
 
+  PNativeUint = ^NativeUInt;
+
+  PValueData = ^TValueData;
+  PValue = ^TValue;
+  PPValue = ^PValue;
+
 var
   RegisteredIgnoreProcs: array of TLeakCheck.TIsInstanceIgnored;
 
-// This is how System releases strings, we'll use similar way to ignore them
-procedure IgnoreManagedFields(const Instance: TObject; ClassType: TClass);
+procedure IgnoreString(P: PString);
+begin
+  if P^ = '' then
+    Exit;
+  if StringRefCount(P^) < 0 then
+    Exit; // Constant string
+
+  RegisterExpectedMemoryLeak(Pointer(PNativeUInt(P)^ - TLeakCheck.StringSkew));
+end;
+
+procedure IgnoreTValue(Value: PValue);
+var
+  ValueData: PValueData absolute Value;
+begin
+  if Value^.IsEmpty then
+    Exit;
+  if Assigned(ValueData^.FValueData) then
+  begin
+    RegisterExpectedMemoryLeak(ValueData^.FValueData as TObject);
+    case Value^.Kind of
+      tkLString, tkUString:
+        IgnoreString(Value^.GetReferenceToRawData);
+    end;
+  end;
+end;
+
+procedure IgnoreArray(P: Pointer; TypeInfo: PTypeInfo; ElemCount: NativeUInt);
+begin
+  if ElemCount = 0 then
+    Exit;
+
+  Assert(ElemCount = 1); // Pure arrays not supported at the moment
+  case TypeInfo^.Kind of
+      tkLString, tkUString:
+        IgnoreString(PString(P));
+      tkRecord:
+        if TypeInfo = System.TypeInfo(TValue) then
+          IgnoreTValue(PValue(P));
+  end;
+end;
+
+procedure IgnoreRecord(P: Pointer; TypeInfo: PTypeInfo);
 var
   I: Cardinal;
   FT: PFieldTable;
+begin
+  FT := PFieldTable(PByte(TypeInfo) + Byte(PTypeInfo(TypeInfo).Name{$IFNDEF NEXTGEN}[0]{$ENDIF}));
+  if FT.Count > 0 then
+  begin
+    for I := 0 to FT.Count - 1 do
+    begin
+{$IFDEF WEAKREF}
+      if FT.Fields[I].TypeInfo = nil then
+        Exit; // Weakref separator
+{$ENDIF}
+      IgnoreArray(Pointer(PByte(P) + IntPtr(FT.Fields[I].Offset)),
+        FT.Fields[I].TypeInfo^, 1);
+    end;
+  end;
+end;
+
+
+// This is how System releases managed fields, we'll use similar way to ignore them
+procedure IgnoreManagedFields(const Instance: TObject; ClassType: TClass);
+var
   InitTable: PTypeInfo;
-  Addr: IntPtr;
 begin
   InitTable := PPointer(PByte(ClassType) + vmtInitTable)^;
   if not Assigned(InitTable) then
     Exit;
 
-  FT := PFieldTable(PByte(InitTable) + Byte(PTypeInfo(InitTable).Name[0]));
-
-  for I := 0 to FT.Count - 1 do
-  begin
-    Addr := IntPtr(PPointer(PByte(Instance) + IntPtr(FT.Fields[I].Offset))^);
-    if FT.Fields[I].TypeInfo^^.Kind in [tkLString, tkUString] then
-      RegisterExpectedMemoryLeak(Pointer(Addr - TLeakCheck.StringSkew));
-  end;
+  IgnoreRecord(Instance, InitTable);
 end;
 
 
 function IgnoreRttiObjects(const Instance: TObject; ClassType: TClass): Boolean;
+var
+  QName: string;
 begin
   // Always use ClassType, it is way safer!
   Result := ClassType.InheritsFrom(TRttiObject);
   if Result then
+    IgnoreManagedFields(Instance, ClassType)
+  else
   begin
-    IgnoreManagedFields(Instance, ClassType);
+    QName := ClassType.QualifiedClassName;
+    Result := (QName = 'System.Rtti.TMethodImplementation.TInvokeInfo')
+      or (QName = 'System.Rtti.TPrivateHeap')
   end;
+end;
+
+function IgnoreCustomAttributes(const Instance: TObject; ClassType: TClass): Boolean;
+begin
+  Result := ClassType.InheritsFrom(TCustomAttribute);
+  if Result then
+    IgnoreManagedFields(Instance, ClassType)
+  else
+    Result := ClassType.QualifiedClassName = 'System.Rtti.TFinalizer';
+end;
+
+function IgnoreAnonymousMethodPointers(const Instance: TObject; ClassType: TClass): Boolean;
+var
+  name: string;
+begin
+  name := ClassType.ClassName;
+  Result := name.StartsWith('MakeClosure$') and name.EndsWith('$ActRec');
 end;
 
 function IgnoreMultipleObjects(const Instance: TObject; ClassType: TClass): Boolean;
@@ -127,6 +230,23 @@ begin
   L := Length(RegisteredIgnoreProcs);
   SetLength(RegisteredIgnoreProcs, L + 1);
   RegisteredIgnoreProcs[L] := Proc;
+end;
+
+procedure AddIgnoreObjectProc(const Procs: array of TLeakCheck.TIsInstanceIgnored);
+var
+  Proc: TLeakCheck.TIsInstanceIgnored;
+begin
+  for Proc in Procs do
+    AddIgnoreObjectProc(Proc);
+end;
+
+procedure IgnoreStrings(const Strings: TStrings);
+var
+  s: string;
+begin
+  RegisterExpectedMemoryLeak(Strings);
+  for s in Strings do
+    IgnoreString(@s);
 end;
 
 {$IFDEF POSIX}
