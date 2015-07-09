shadow-cs/delphi-leakcheck

commit 6aab9f0403abef3afea2fd1ba071214d959ce38b

fixed AV (and assertion that hides it) when exploring fields with unknown type reference

Source/LeakCheck.Cycle.pas
@@ -477,6 +477,7 @@ procedure TScanner.ScanClassInternal(const Instance: TObject);
       for I := 0 to I - 1 do
       begin
         with PFieldExEntry(P)^ do
+          if Assigned(TypeRef) then
         begin
           if not TArray.BinarySearch<Cardinal>(Classic, Offset, Found, Comparer) then
             ScanArray(Pointer(PByte(Inst) + NativeInt(Offset)), TypeRef^, 1, @Name);
@@ -606,6 +607,7 @@ procedure TScanner.ScanRecord(P: Pointer; TypeInfo: PTypeInfo);
     for I := 0 to Count - 1 do
     begin
       with PRecordTypeField(P)^ do
+        if Assigned(Field.TypeRef) then
       begin
         ScanArray(Pointer(PByte(Inst) + NativeInt(Field.FldOffset)),
           Field.TypeRef^, 1, @Name);

commit 7ce4231b1f789fd5f453e72108609f11a128bfda

fixed a bug introduced in 6aab9f0 when exploring fields with unknown type reference will fail to skip the name and corrupt the scanning process

Source/LeakCheck.Cycle.pas
@@ -477,10 +477,12 @@ procedure TScanner.ScanClassInternal(const Instance: TObject);
       for I := 0 to I - 1 do
       begin
         with PFieldExEntry(P)^ do
-          if Assigned(TypeRef) then
         begin
-          if not TArray.BinarySearch<Cardinal>(Classic, Offset, Found, Comparer) then
+          if Assigned(TypeRef) and not TArray.BinarySearch<Cardinal>(
+            Classic, Offset, Found, Comparer) then
+          begin
             ScanArray(Pointer(PByte(Inst) + NativeInt(Offset)), TypeRef^, 1, @Name);
+          end;
           // AttrData - skip the name
           P := PByte(@Name);
           Inc(P, P^ + 1);
@@ -607,10 +609,12 @@ procedure TScanner.ScanRecord(P: Pointer; TypeInfo: PTypeInfo);
     for I := 0 to Count - 1 do
     begin
       with PRecordTypeField(P)^ do
-        if Assigned(Field.TypeRef) then
       begin
-        ScanArray(Pointer(PByte(Inst) + NativeInt(Field.FldOffset)),
-          Field.TypeRef^, 1, @Name);
+        if Assigned(Field.TypeRef) then
+        begin
+          ScanArray(Pointer(PByte(Inst) + NativeInt(Field.FldOffset)),
+            Field.TypeRef^, 1, @Name);
+        end;
         // AttrData - skip the name
         P := PByte(@Name);
         Inc(P, P^ + 1);